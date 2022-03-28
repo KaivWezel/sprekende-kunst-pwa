d6c77eaf18179521c7d1c194f22128fea7dd7b0c
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-03-28 15:20:19 +0200

    Started serviceworker

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
 console.log("appje");
+
+if ("serviceWorker" in navigator) {
+	navigator.serviceWorker
+		.register("/sw.js")
+		.then((reg) => {
+			console.log("serviceworker registered", reg);
+			console.log("scope:", reg.scope);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+}
---
+++ public/sw.js
@@ -0,0 +1,20 @@
+// intall service wokrer
+self.addEventListener("install", (event) => {
+	event.waitUntil(
+		addToCache(["/styles/index.css", "/js/app.js", "/img/icons"])
+	);
+	console.log("serviceworker installed");
+});
+
+self.addEventListener("activate", () => {
+	console.log("activated");
+});
+
+self.addEventListener("fetch", (evt) => {
+	console.log("fetch", evt);
+});
+
+const addToCache = async (resources) => {
+	const cache = await caches.open("sprekende-kunst");
+	await cache.addAll(resources);
+};

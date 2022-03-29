137690d905c7348eec408ba89edf0105ebcfe535
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-03-29 17:20:16 +0200

    Added rollup config for dist build

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,16 @@
-// rollup config file
+// rollup config
+
+import copy from "rollup-plugin-copy";
+
 export default {
+	input: "index.js",
 	output: {
 		file: "dist/bundle.js",
 		format: "cjs",
 	},
+	plugins: [
+		copy({
+			targets: [{ src: ["public/styles", "public/img"], dest: "dist" }],
+		}),
+	],
 };
